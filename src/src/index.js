b4027fb9e211f7d8349c497a237e63c95482cfdb
restuta <restuta8@gmail.com>
2017-11-14 21:24:46 -0800

    initial setup

---
+++ src/src/index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk')
+const config = require('./config')
+
+console.log(chalk.yellow('Your github token is:'))
+
+console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
