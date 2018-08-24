82aad65c168d0e49427410ffae26bf32025fc282
Stephen Kang <kang.stephen94@gmail.com>
2018-08-23 23:45:54 -0700

    Configure api

--- src/example.js
+++ src/example.js
@@ -12,8 +12,8 @@ const http = axios.create({
 
 async function printCurrentUserDetails() {
   try {
-    const response = await http.get('/user')
-    console.dir(response.data, { colors: true, depth: 4 })
+    const response = await http.get(`/repos/kangstephen94/wallstreetbets/commits?per_page=50`)
+    console.dir(response.data.length, { colors: true, depth: 4 })
   } catch (err) {
     console.error(chalk.red(err))
     console.dir(err.response.data, { colors: true, depth: 4 })
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ const config = require('./config')
 console.log(chalk.yellow('Your github token is:'))
 console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
 
+console.log(chalk.red('Command line options'));
+process.argv.forEach(function(val, index, array) {
+  console.log(index + ': ' + val)
+})
 // remove this line
 require('./example')
