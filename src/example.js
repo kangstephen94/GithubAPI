7d9dca7ba11e72c36fbcc8a7f0c2cff93c082078
Stephen Kang <kang.stephen94@gmail.com>
2018-08-28 16:38:11 -0700

    Add progress indicator

--- src/example.js
+++ src/example.js
@@ -10,10 +10,52 @@ const http = axios.create({
   },
 })
 
+// Pluck out the node command line options using regex.
+let repoName
+let timePeriod
+let userName
+let repoArg
+
+for (let i = 0; i < process.argv.length; i++) {
+  if (process.argv[i] === '--repo') {
+    repoArg = process.argv[i + 1]
+    userName = repoArg.match(/[^ /]*/)[0]
+    repoName = repoArg.match(/(?!.*\/).+/)[0]
+  } else if (process.argv[i] === '--period') {
+    timePeriod = process.argv[i + 1]
+  }
+}
+
+// Progress Indicator
+
+// var ProgressBar = require('progress')
+
+// var bar = new ProgressBar(':bar', { total: 50 })
+
+// function startProgress () {
+//   var timer = setInterval(function() {
+//     bar.tick()
+//     if (bar.complete) {
+//       console.log('\ncomplete\n')
+//       clearInterval(timer)
+//     }
+//   }, 100)
+// }
+
+console.log(chalk.green(`Fetching comments for past ${timePeriod} for "${repoArg}"...`))
+
 async function printCurrentUserDetails() {
   try {
-    const response = await http.get(`/repos/kangstephen94/wallstreetbets/commits?per_page=50`)
-    console.dir(response.data.length, { colors: true, depth: 4 })
+    // startProgress()
+    let response = []
+    let totalResponses = 0
+
+    while (response.length === 30) {
+      const response = await http.get(`/repos/${userName}/${repoName}/comments`)
+      const dataLength = response.data.length
+      totalResponses = totalResponses + dataLength
+      console.dir(response.data, { colors: true, depth: 4 })
+    }
   } catch (err) {
     console.error(chalk.red(err))
     console.dir(err.response.data, { colors: true, depth: 4 })
